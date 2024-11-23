ea616bd1f6d270186fe9c22e146cb00cb60ae630
Mateusz Kulesza <ev45ive@gmail.com>
2024-11-23 15:00:50 +0100

    // Extract Locator - Named Recipe

--- tests/website/website.spec.ts
+++ tests/website/website.spec.ts
@@ -5,17 +5,19 @@ test("Test page header", async ({ page }) => {
   // pwgoto
   await page.goto("https://playwright.dev/");
 
-// CSS exact selector - fragile!
-// await page.$('#__docusaurus_skipToContent_fallback > header > div > h1');
+  // CSS exact selector - fragile!
+  // await page.$('#__docusaurus_skipToContent_fallback > header > div > h1');
 
+  // Web-First / User-Centric
+  await expect(page.getByRole("heading")).toHaveText(
+    "Playwright enables reliable"
+  );
 
-// Web-First / User-Centric 
+  // Extract Locator - Named Recipe
+  const TopHeader = page.getByRole("heading", {
+    name: "Playwright enables reliable",
+  });
 
-await expect(page.getByRole('heading'))
-            .toHaveText('Playwright enables reliable');
-
-// Future-Proof
-await expect(page.getByRole('heading', { name: 'Playwright enables reliable' }))
-            .toBeVisible()
-
+  // Future-Proof
+  await expect(TopHeader).toBeVisible();
 });
