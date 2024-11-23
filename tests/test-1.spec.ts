85e865a562639f5c220edc5bf137fca4478aa378
Mateusz Kulesza <ev45ive@gmail.com>
2024-11-23 17:10:13 +0100

    Keyboard shortcuts

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -3,9 +3,11 @@ import { test, expect } from "@playwright/test";
 test("test", async ({ page }) => {
   await page.goto("https://playwright.dev/");
 
+  const SearchBox = page.getByPlaceholder("Search docs");
+
   await page.getByLabel("Search").click();
-  await page.getByPlaceholder("Search docs").click();
-  await page.getByPlaceholder("Search docs").fill("actions ");
+  await SearchBox.click();
+  await SearchBox.fill("actions ");
 
   await page.getByRole("link", { name: "Actions", exact: true }).click();
 
@@ -13,4 +15,20 @@ test("test", async ({ page }) => {
   await expect(page).toHaveURL(/docs\/input/);
 
   await expect(page.locator("h1")).toContainText("Actions");
+});
+
+test("Keyboard shortcuts", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+  const SearchBox = page.getByPlaceholder("Search docs");
+
+  await page.locator("body").press("Control+K");
+  await SearchBox.pressSequentially("Actions", { delay: 500 });
+
+  // await expect(SearchBox).toHaveValue('Actions')
+  const value = await SearchBox.inputValue();
+  if (!value) {
+    await SearchBox.pressSequentially("Actions", { delay: 500 });
+  }
+
+  await SearchBox.press("Enter");
 });
