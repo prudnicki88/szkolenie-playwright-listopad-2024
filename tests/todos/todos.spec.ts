7ed0fca8aa4b120fa8a0e8f4da9383b47b9a91f5
Mateusz Kulesza <ev45ive@gmail.com>
2024-11-24 14:11:58 +0100

    Chaining locator - children change "context" from page to parent locator

--- tests/todos/todos.spec.ts
+++ tests/todos/todos.spec.ts
@@ -50,33 +50,32 @@ test.describe("todos", { tag: ["@todos"] }, () => {
       const TodoInput = page.getByPlaceholder("What needs to be done?");
       const TodoItem = page.getByTestId("todo-item");
 
+      // Arrange
       await expect(TodoCounter).toHaveText("3 items left");
       await expect(TodoItem).toHaveCount(3);
 
+      // Arrange
       await TodoInput.fill("Here is my TODO");
       await TodoInput.press("Enter");
 
+      // Arrange
       await expect(TodoCounter).toHaveText("4 items left");
       await expect(TodoItem).toHaveCount(4);
     });
 
     test("Removing todos", async ({ page }) => {
-      // test.setTimeout(50000) // debugg
-
+      // Arrange
       const TodoItem = page.getByTestId("todo-item");
       const TodoDelete = page.getByRole("button", { name: "Delete" });
       const TodoCounter = page.getByTestId("todo-count");
-
-      // debugger; // breakpoint
-
       const SelectedTodo = TodoItem.first();
-
       await expect(TodoItem).toHaveCount(3);
+
+      // Act
       await SelectedTodo.hover();
-
       await SelectedTodo.locator(TodoDelete).click();
-      // .click({force:true}); // Not User-Centric!
 
+      // Assert
       await expect(TodoItem).toHaveCount(2);
     });
   });
@@ -86,6 +85,24 @@ test.describe("todos", { tag: ["@todos"] }, () => {
 
     test("Shows all todos", async ({ page }) => {
       const TodoItem = page.getByTestId("todo-item");
+      await expect(TodoItem).toHaveCount(3);
+    });
+
+    test("Completing todo", async ({ page }) => {
+      const TodoItem = page.getByTestId("todo-item");
+      const TodoTitle = page.getByTestId("todo-title");
+      const ToggleTodo = page.getByLabel("Toggle Todo");
+      const BananaTodo = TodoTitle.getByText("banana");
+
+      const SelectedTodo = TodoItem.filter({
+        has: BananaTodo,
+      });
+
+      // await ToggleTodo.click(); // context=Page
+      // await SelectedTodo.getByLabel("Toggle Todo").click(); // Context=SelectedTodo
+      // await SelectedTodo.locator(page.getByLabel("Toggle Todo")).click(); // Context=SelectedTodo
+      await SelectedTodo.locator(ToggleTodo).click(); // Context=SelectedTodo
+
       await expect(TodoItem).toHaveCount(3);
     });
   });
