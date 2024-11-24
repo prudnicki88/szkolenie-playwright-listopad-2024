e9e8a89b496b3b8dd8ce46ff55100c5e6a54b999
Mateusz Kulesza <ev45ive@gmail.com>
2024-11-24 17:16:04 +0100

    Api request, toEqual arrayContaining, objectContaining, expect.any(String),

--- tests/search/login-api.spec.ts
+++ tests/search/login-api.spec.ts
@@ -1,9 +1,16 @@
-import test from "@playwright/test";
+import test, { expect } from "@playwright/test";
 
 test("API Login test", async ({ request }) => {
-  const response = await request.get("http://localhost:3000/api/token", {
+  const response = await request.get("http://localhost:3000/todos.json", {
     params: {},
   });
 
-  
+  expect(await response.json()).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        title: expect.any(String),
+        completed: expect.any(Boolean),
+      }),
+    ])
+  );
 });
